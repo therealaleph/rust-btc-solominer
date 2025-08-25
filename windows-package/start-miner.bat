59559591685c15d2f4d5f935b226fefe546f39af
Shin (Former Aleph) <67456590+therealaleph@users.noreply.github.com>
2025-08-26 01:53:39 +0300

    Delete windows-package directory

--- windows-package/start-miner.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-title Bitcoin Solo Miner
-echo Starting Bitcoin Solo Miner...
-echo.
-echo Make sure you have configured your wallet address in config.ini
-echo.
-pause
-bitcoin-solo-miner.exe
-pause
